5a2c9d79b64315e4de9da12bbdb62b10f3d52262
Innovativenut <damodara.abhiram@gmail.com>
2021-09-21 12:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ package main
 func Bar(s string) string { return "Hello, " + s }
 func exec() string { return Bar("World") }
 `;
-fs.readFile('./main.wasm', async (err, data) => {
-	WebAssembly.instantiateStreaming = async (resp, importObject) => {
-		const source = await WebAssembly.compile(resp);
-		return await WebAssembly.instantiate(source, importObject);
-	};
+fetch(
+  'https://github.com/Innovativenut/wasm-with-go/blob/master/main.wasm?raw=true'
+)
+  .then(async data => {
+    WebAssembly.instantiateStreaming = async (resp, importObject) => {
+      const source = await WebAssembly.compile(resp);
+      return await WebAssembly.instantiate(source, importObject);
+    };
 
-	const go = new Go();
-	const result = await WebAssembly.instantiateStreaming(data, go.importObject);
-	go.run(result);
-	console.log(printMessage(src, 'exec'));
-});
+    const go = new Go();
+    const result = await WebAssembly.instantiateStreaming(
+      data,
+      go.importObject
+    );
+    go.run(result);
+    console.log(printMessage(src, 'exec'));
+  })
+  .catch();
